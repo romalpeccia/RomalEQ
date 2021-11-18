9d259089d8b11ee46516a701abe44fd960303572
romalpeccia <romal.peccia@gmail.com>
2021-11-18 14:24:31 -0500

    added slider parameters

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -53,6 +53,11 @@ public:
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
+
+    //NON DEFAULT CODE BELOW
+    static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+    juce::AudioProcessorValueTreeState apvts{ *this, nullptr, "Parameters", createParameterLayout() };
+
 private:
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (RomalEQAudioProcessor)
